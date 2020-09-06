fd38c4c97530d54de83f91341e4bd75f9b066cbd
oksujin <ok0579@naver.com>
2020-09-06 13:18:51 +0000

    is it okay?

--- Biggner/test.py
+++ Biggner/test.py
@@ -3,4 +3,12 @@
 a = [i for i in range(20) if i % 2 == 1]
 print (a)
 
-# 2.
+# 2. 1부터 9까지의 수의 제곱 값을 포함하는 리스트
+b = [i*i for i in range (1, 9)]
+print(b)
+
+# 3. N*M 크기의 2차원 리스트 초기화하기
+N = 3
+M = 4
+array = [[0] * M for _ in range(N)]
+print(array)
